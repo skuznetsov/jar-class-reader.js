c2325b60d47ab087e0d4734d2ba3b4c975cafb38
Sergey Kuznetsov <sergey@iterudit.com>
2025-02-05 14:11:39 -0500

    Small code refactoring while neat it up.

--- app.js
+++ app.js
@@ -4,103 +4,101 @@ const JavaEmulator = require('./lib/emulator');
 let ancestry = {};
 let dependency = {};
 
-function includes (array, element) {
-    return array.indexOf(element) > -1;
+class ClassProcessor {
+    classLoader = null;
+    dependency = {};
+    ancestry = {};
+    classesToProcess = null;
+
+    recordRelationship(thisClass, thatClass, callingField = "calling", calledByField = "calledBy") {
+        if (!thatClass) {
+            return;
+        }
+        if (this.classLoader.isSimpleArray(thatClass)) {
+            return;
+        }
+        thatClass = thatClass.replace(/^\[*L|;$/g, '');
+        if(thisClass == thatClass || thatClass.startsWith("java")) {
+            return;
+        }
+        this.classLoader.markJarInUse(thatClass);
+        let demangledThisClass = thisClass.replace(/\//g,'.');
+        let demangledThatClass = thatClass.replace(/\//g,'.');
+
+        if (!this.classLoader.classes[thatClass]) {
+            if (!this.classesToProcess.includes(thatClass)) {
+                this.classesToProcess.push(thatClass);
+            }
+            this.classLoader.getClass(thatClass, true);
+        }
+
+        let thisJar = this.classLoader.getJarNameByClassName(thisClass) || 'application';
+        let thatJar = this.classLoader.getJarNameByClassName(thatClass);
+
+        if (thatJar) {
+            this.dependency[thisJar] ||= {uses: []};
+            if (!this.dependency[thisJar].uses.includes(thatJar)) {
+                this.dependency[thisJar].uses.push(thatJar);
+            }
+        }
+
+        this.ancestry[demangledThisClass] ||= {};
+        this.ancestry[demangledThisClass][callingField] ||= {};
+        this.ancestry[demangledThisClass][callingField][demangledThatClass] = true;
+
+        this.ancestry[demangledThatClass] ||= {};
+        this.ancestry[demangledThatClass][calledByField] ||= {};
+        this.ancestry[demangledThatClass][calledByField][demangledThisClass] = true;
+
+    }
+
+    async process(dirName) {
+        console.time('bigClassLoad');
+        this.classLoader = new ClassLoader();
+        await this.classLoader.loadAllJarFiles(dirName);
+        console.timeEnd('bigClassLoad');
+
+        // process.exit(0);
+        let processedClasses = 0;
+        this.classesToProcess = Object.keys(this.classLoader.classes);
+        console.log(`Loaded ${this.classesToProcess.length} classes...`)
+
+        while(this.classesToProcess.length) {
+            let className = this.classesToProcess.shift();
+            processedClasses++;
+            let ca = await this.classLoader.getClass(className, true);
+            if (!ca) {
+                continue;
+            }
+
+            if (processedClasses % 100 == 0) {
+                this.classLoader.logWarning(`Done: ${processedClasses}, Left: ${this.classesToProcess.length}, Now: ${className}`);
+            }
+
+            this.recordRelationship(className, ca.superClassName, "inheritedFrom", "inheritedBy");
+
+            for (let method of ca.methods) {
+                let emulator = new JavaEmulator(this.classLoader, ca);
+                await emulator.emulateMethod(method);
+            }
+            let externalClasses = ca.externalClasses;
+
+            if (externalClasses) {
+                for (let extClass of externalClasses) {
+                    this.recordRelationship(className, extClass, "calling", "calledBy");
+                }
+            }
+
+
+        }
+
+
+        console.log(JSON.stringify(this.ancestry, null, 2));
+        console.log('JAR dependency: ' + JSON.stringify(this.dependency, null, 2));
+        console.log('JARs status: ' + JSON.stringify(this.classLoader.jars, null, 2));
+    }
 }
 
-function markCalledClass(classLoader, thisClass, thatClass) {
-    if (!thatClass) {
-        return;
-    }
-    if (classLoader.isSimpleArray(thatClass)) {
-        return;
-    }
-    thatClass = thatClass.replace(/^\[*L|;$/g, '');
-    if(thisClass == thatClass || thatClass.startsWith("java")) {
-        return;
-    }
-    classLoader.markJarInUse(thatClass);
-    let demangledThisClass = thisClass.replace(/\//g,'.');
-    let demangledThatClass = thatClass.replace(/\//g,'.');
+let processor = new ClassProcessor();
 
-    if (!loader.classes[thatClass]) {
-        if (!includes(classes, thatClass)) {
-            classes.push(thatClass);
-        }
-        loader.getClass(thatClass, true);
-    }
-
-    let thisJar = loader.getJarNameByClassName(thisClass) || 'application';
-    let thatJar = loader.getJarNameByClassName(thatClass);
-
-    if (thatJar) {
-        dependency[thisJar] = dependency[thisJar] || {uses: []};
-        if (!includes(dependency[thisJar].uses, thatJar)) {
-            dependency[thisJar].uses.push(thatJar);
-        }
-    }
-
-    ancestry[demangledThisClass] = ancestry[demangledThisClass] || {calling: {}};
-    ancestry[demangledThisClass].calling = ancestry[demangledThisClass].calling || {};
-    ancestry[demangledThisClass].calling[demangledThatClass] = true;
-
-    ancestry[demangledThatClass] = ancestry[demangledThatClass] || {calledBy: {}};
-    ancestry[demangledThatClass].calledBy = ancestry[demangledThatClass].calledBy || {};
-    ancestry[demangledThatClass].calledBy[demangledThisClass] = true;
-
-}
-
-console.time('bigClassLoad');
-let amazonS3 = loader.getClass("com/amazonaws/services/s3/AmazonS3Client");
-console.timeEnd('bigClassLoad');
-
-// process.exit(0);
-let processedClasses = 0;
-let classes = Object.keys(loader.classes);
-console.log(`Loaded ${classes.length} classes...`)
-
-while(classes.length) {
-    let className = classes.shift();
-    processedClasses++;
-    let demangledClassName = className.replace(/\//g,'.');
-    let ca = loader.getClass(className, true);
-    if (!ca) {
-        continue;
-    }
-
-    // if (processedClasses % 100 == 0) {
-        loader.logWarning(`Done: ${processedClasses}, Left: ${classes.length}, Now: ${className}`);
-    // }
-
-    let parentClassName = ca.getSuperClassName().replace(/\//g,'.');
-
-    // TODO: Add superclass to ancestry
-    ancestry[demangledClassName] = ancestry[demangledClassName] || {inheritedFrom: []};
-    ancestry[demangledClassName].inheritedFrom = ancestry[demangledClassName].inheritedFrom || [];
-    ancestry[demangledClassName].inheritedFrom.push(parentClassName);
-
-    ancestry[parentClassName] = ancestry[parentClassName] || {inheritedBy: []};
-    ancestry[parentClassName].inheritedBy = ancestry[parentClassName].inheritedBy || [];
-    ancestry[parentClassName].inheritedBy.push(demangledClassName);
-
-    let thisJar = loader.getJarNameByClassName(className) || 'application';
-    let thatJar = loader.getJarNameByClassName(parentClassName);
-
-    if (thatJar) {
-        dependency[thisJar] = dependency[thisJar] || {uses: []};
-        if (!includes(dependency[thisJar].uses, thatJar)) {
-            dependency[thisJar].uses.push(thatJar);
-        }
-    }
-
-    let externalClasses = ca.getExternalClasses();
-
-    externalClasses && externalClasses.forEach(extClass => {
-        markCalledClass(loader, className, extClass);
-    });
-}
-
-
-console.log(JSON.stringify(ancestry, null, 2));
-console.log('JAR dependency: ' + JSON.stringify(dependency, null, 2));
-console.log('JARs status: ' + JSON.stringify(loader.jars, null, 2));
+processor.process("./data/");
